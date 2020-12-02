4bb7e4554262822eaf471d378cdaba5f9aceb571
denipriyadi18 <denipriyadi90@gmail.com>
2020-12-02 22:23:51 +0700

    Tugas Angka

---
+++ Analisis dan Desain Algoritma/Tugas Angka.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+	
+	int i, b;
+	int Data[7]={1,2,2,5,4,4,1};
+	reverse(Data, Data + 7); 
+			
+	for (i=6; i>=0; i--){
+		cout << Data[i] ;
+			for(b=1; b<=i; b++){
+				cout << "0" ;
+			}
+		cout << endl;
+	}	
+}
